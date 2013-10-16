7efbfeb97563cb6da78e0012e954e96b4d46db37
andre de oliveira <deoliveirambx@googlemail.com>
2013-10-16 01:32:43 -0400

    progress in httpd demo

--- demos/httpd/httpd.cpp
+++ demos/httpd/httpd.cpp
@@ -1,5 +1,16 @@
 #include "httpd.h"
+#include <iostream>
 
 Httpd::Httpd() : Daemon() { }
 
 Httpd::~Httpd() { }
+
+void Httpd::run()
+{
+    std::cout << "Starting..." << std::endl;
+}
+
+void Httpd::handleRequest(uv_stream_t *, ssize_t, uv_buf_t)
+{
+    std::cout << "Master I'm here to serve you" << std::endl;
+}
--- demos/httpd/httpd.h
+++ demos/httpd/httpd.h
@@ -8,6 +8,8 @@ class Httpd : public Daemon
 public:
     Httpd();
     virtual ~Httpd();
+    void run();
+    void handleRequest(uv_stream_t *, ssize_t, uv_buf_t);
 };
 
 #endif // !HTTPD_H
